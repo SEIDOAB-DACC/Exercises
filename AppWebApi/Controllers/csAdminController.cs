2a5d8956a568263defc7250c0949d70fa97b5e2d
Martin <martin_lenart@icloud.com>
2024-08-30 15:30:58 +0200

    Exercise 1 done

--- AppWebApi/Controllers/csAdminController.cs
+++ AppWebApi/Controllers/csAdminController.cs
@@ -19,6 +19,8 @@ namespace AppWebApi.Controllers
     [Route("api/[controller]/[action]")]
     public class csAdminController : Controller
     {
+        private const string seedSource = "./friends-seeds.json";
+
         //GET: api/musicgroups/seed?count={count}
         [HttpGet()]
         [ActionName("Info")]
@@ -29,6 +31,31 @@ namespace AppWebApi.Controllers
             try
             {
                 return Ok(csAppConfig.Address);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+           
+        }
+
+        [HttpGet()]
+        [ActionName("AfricanAnimals")]
+        [ProducesResponseType(200, Type = typeof(csAnimal))]
+        [ProducesResponseType(400, Type = typeof(string))]
+        public async Task<IActionResult> AfricanAnimals(string count)
+        {
+            try
+            {
+                int _count = int.Parse(count);
+
+                var fn = Path.GetFullPath(seedSource);
+                var _seeder = new csSeedGenerator(fn);
+
+                //var animal = new csAnimal().Seed(_seeder);
+
+                var animals = _seeder.ItemsToList<csAnimal>(_count);
+                return Ok(animals);
             }
             catch (Exception ex)
             {
---
+++ Models/csAnimal.cs
@@ -0,0 +1,29 @@
+using Configuration;
+using Seido.Utilities.SeedGenerator;
+
+namespace Models;
+
+public enum AnimalKind {Zebra, Elephant, Lion, Leopard, Gasell}
+public class csAnimal :ISeed<csAnimal>
+{
+    public AnimalKind Kind { get; set; }
+    public int Age { get; set; }
+    public string Name { get; set; }
+    public string Description { get; set; }
+
+    #region seeder
+    public bool Seeded { get; set; } = false;
+
+    public csAnimal Seed (csSeedGenerator _seeder)
+    {
+        Seeded = true;
+        Kind = _seeder.FromEnum<AnimalKind>();
+        Age = _seeder.Next(0, 11);
+        Name = _seeder.PetName;
+        Description = _seeder.LatinSentence;
+
+        return this;
+    }
+    #endregion
+    
+}
